5c55cfb52b308714e727d602f0ca1952d345dd1e
occlusion <allenmbrower@gmail.com>
2023-12-09 15:33:53 -0500

    moved param classes

--- bot.py
+++ bot.py
@@ -128,6 +128,40 @@ async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
 
 
+@dataclass
+class PromptParams:
+    prompt: str = None
+    negative_prompt: str = None
+
+
+@dataclass
+class ModelParams:
+    model: str = None
+    lora: Choice[str] = None
+    lora_strength: float = 1.0
+    lora2: Choice[str] = None
+    lora_strength2: float = 1.0
+    lora3: Choice[str] = None
+    lora_strength3: float = 1.0
+
+
+@dataclass
+class ImageParams:
+    aspect_ratio: str = None
+
+
+@dataclass
+class SamplerParams:
+    num_steps: Range[int, 1, 20] = None
+    cfg_scale: Range[float, 1.0, 10.0] = None
+    seed: int = None
+
+
+@dataclass
+class WorkflowParams:
+    config: str = None
+
+
 class ImageButton(discord.ui.Button):
     def __init__(self, label, emoji, row, callback):
         super().__init__(label=label, style=discord.ButtonStyle.grey, emoji=emoji, row=row)
@@ -554,40 +588,6 @@ async def slash_command(
                      sampler_params,
                      workflow_params
                      )
-
-
-@dataclass
-class PromptParams:
-    prompt: str = None
-    negative_prompt: str = None
-
-
-@dataclass
-class ModelParams:
-    model: str = None
-    lora: Choice[str] = None
-    lora_strength: float = 1.0
-    lora2: Choice[str] = None
-    lora_strength2: float = 1.0
-    lora3: Choice[str] = None
-    lora_strength3: float = 1.0
-
-
-@dataclass
-class ImageParams:
-    aspect_ratio: str = None
-
-
-@dataclass
-class SamplerParams:
-    num_steps: Range[int, 1, 20] = None
-    cfg_scale: Range[float, 1.0, 10.0] = None
-    seed: int = None
-
-
-@dataclass
-class WorkflowParams:
-    config: str = None
 
 
 async def do_request(
